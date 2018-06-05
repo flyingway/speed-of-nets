7e27d1bb7c7f5b53d57f77e65bb4b3c6ab2b5d59
flyingway <294771982@qq.com>
2018-06-05 22:38:21 +0800

    all of program
    soso

---
+++ Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.锁定位置ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.归位ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.记住位置ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.自由门ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WIFI共享ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.P2PToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.高级设置ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.选择网络ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.保存流量数据ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.点击穿透ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ping检测ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.开机启动ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.重启ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.关闭ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Ping1 = New System.Net.NetworkInformation.Ping()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(368, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(56, 21)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(368, 40)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(56, 21)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Gold
+        Me.Label1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(0, 1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(166, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "↑ 上传速度:"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.锁定位置ToolStripMenuItem, Me.归位ToolStripMenuItem, Me.记住位置ToolStripMenuItem, Me.ToolStripSeparator1, Me.自由门ToolStripMenuItem, Me.WIFI共享ToolStripMenuItem, Me.P2PToolStripMenuItem, Me.ToolStripSeparator2, Me.高级设置ToolStripMenuItem, Me.重启ToolStripMenuItem, Me.关闭ToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(127, 214)
+        '
+        '锁定位置ToolStripMenuItem
+        '
+        Me.锁定位置ToolStripMenuItem.Name = "锁定位置ToolStripMenuItem"
+        Me.锁定位置ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.锁定位置ToolStripMenuItem.Text = "锁定位置"
+        '
+        '归位ToolStripMenuItem
+        '
+        Me.归位ToolStripMenuItem.Name = "归位ToolStripMenuItem"
+        Me.归位ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.归位ToolStripMenuItem.Text = "归零位置"
+        '
+        '记住位置ToolStripMenuItem
+        '
+        Me.记住位置ToolStripMenuItem.Name = "记住位置ToolStripMenuItem"
+        Me.记住位置ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.记住位置ToolStripMenuItem.Text = "记住位置"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(123, 6)
+        '
+        '自由门ToolStripMenuItem
+        '
+        Me.自由门ToolStripMenuItem.Name = "自由门ToolStripMenuItem"
+        Me.自由门ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.自由门ToolStripMenuItem.Text = "自由门"
+        '
+        'WIFI共享ToolStripMenuItem
+        '
+        Me.WIFI共享ToolStripMenuItem.Name = "WIFI共享ToolStripMenuItem"
+        Me.WIFI共享ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.WIFI共享ToolStripMenuItem.Text = "WIFI共享"
+        '
+        'P2PToolStripMenuItem
+        '
+        Me.P2PToolStripMenuItem.Name = "P2PToolStripMenuItem"
+        Me.P2PToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.P2PToolStripMenuItem.Text = "P2P"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(123, 6)
+        '
+        '高级设置ToolStripMenuItem
+        '
+        Me.高级设置ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.选择网络ToolStripMenuItem, Me.保存流量数据ToolStripMenuItem, Me.点击穿透ToolStripMenuItem, Me.Ping检测ToolStripMenuItem, Me.开机启动ToolStripMenuItem})
+        Me.高级设置ToolStripMenuItem.Name = "高级设置ToolStripMenuItem"
+        Me.高级设置ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.高级设置ToolStripMenuItem.Text = "高级设置"
+        '
+        '选择网络ToolStripMenuItem
+        '
+        Me.选择网络ToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.选择网络ToolStripMenuItem.Name = "选择网络ToolStripMenuItem"
+        Me.选择网络ToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.选择网络ToolStripMenuItem.Text = "选择监视网络"
+        Me.选择网络ToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        '保存流量数据ToolStripMenuItem
+        '
+        Me.保存流量数据ToolStripMenuItem.Name = "保存流量数据ToolStripMenuItem"
+        Me.保存流量数据ToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.保存流量数据ToolStripMenuItem.Text = "保存流量数据"
+        '
+        '点击穿透ToolStripMenuItem
+        '
+        Me.点击穿透ToolStripMenuItem.Name = "点击穿透ToolStripMenuItem"
+        Me.点击穿透ToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.点击穿透ToolStripMenuItem.Text = "点击穿透"
+        '
+        'Ping检测ToolStripMenuItem
+        '
+        Me.Ping检测ToolStripMenuItem.Name = "Ping检测ToolStripMenuItem"
+        Me.Ping检测ToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.Ping检测ToolStripMenuItem.Text = "Ping检测"
+        '
+        '开机启动ToolStripMenuItem
+        '
+        Me.开机启动ToolStripMenuItem.Name = "开机启动ToolStripMenuItem"
+        Me.开机启动ToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.开机启动ToolStripMenuItem.Text = "开机启动"
+        '
+        '重启ToolStripMenuItem
+        '
+        Me.重启ToolStripMenuItem.Name = "重启ToolStripMenuItem"
+        Me.重启ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.重启ToolStripMenuItem.Text = "重启"
+        '
+        '关闭ToolStripMenuItem
+        '
+        Me.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem"
+        Me.关闭ToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.关闭ToolStripMenuItem.Text = "关闭"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Gold
+        Me.Label2.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.Location = New System.Drawing.Point(0, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(166, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "↓ 下载速度:"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Gold
+        Me.Label4.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label4.Location = New System.Drawing.Point(0, 53)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(166, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "下载总流量："
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "网络监控"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(166, 72)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Opacity = 0.9R
+        Me.ShowInTaskbar = False
+        Me.Text = "网速监控"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents 重启ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 关闭ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 归位ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 锁定位置ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents 自由门ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents P2PToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents WIFI共享ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Ping1 As System.Net.NetworkInformation.Ping
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents 记住位置ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents 高级设置ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 选择网络ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 点击穿透ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Ping检测ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 开机启动ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 保存流量数据ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,326 @@
+﻿Imports System.Net.NetworkInformation
+Imports System.Diagnostics
+Imports Microsoft.Win32
+Imports System.Drawing.Printing
+Imports System.Net
+
+Public Class Form1
+    Public savedata As Single = 0
+    Public netlist As Short = My.Settings.netselect   '网络适配器选择
+
+#Region "点击穿透API及常量"
+    Declare Function SetWindowLongA Lib "user32" (ByVal hwnd As IntPtr, ByVal nIndex As Integer, ByVal dwNewLong As Integer) As Integer
+    Declare Function GetWindowLongA Lib "user32" (ByVal hwnd As Integer, ByVal nIndex As Integer) As Integer
+    Const WS_EX_TRANSPARENT = &H20&
+    Const GWL_EXSTYLE = (-20)
+    Const WS_EX_LAYERED = &H80000
+#End Region
+
+
+#Region "网速监控"
+    Dim Thread1 As New System.Threading.Thread(AddressOf network)
+    Dim myNA() As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces
+    Public start As Long           '程序启动时接受到总数据（不变）
+    Public middata As Long         '下载流量中间介质
+
+    Sub network()
+        Do
+
+            Dim data As IPv4InterfaceStatistics = myNA(netlist).GetIPv4Statistics
+            Dim s1 As String = "  ↓ 下载速度：  " + Format((data.BytesReceived - Val(TextBox1.Text)) / 1024, "###0.00") & " KB/s"
+            Dim s2 As String = "  ↑ 上传速度：  " + Format((data.BytesSent - Val(TextBox2.Text)) / 1024, "###0.00") & " KB/s"
+            If data.BytesReceived = 0 Then
+                start = 0
+                Exit Sub
+            End If
+            TextBox1.Text = data.BytesReceived
+            TextBox2.Text = data.BytesSent
+
+            '以下为总流量记数
+            Label1.Text = s1
+            Label2.Text = s2
+
+            Label4.Text = "  下载总流量：  " + Format((data.BytesReceived - start + savedata * 1024 * 1024) / 1024 / 1024, "###0.00") & " MB"
+
+            middata = (data.BytesReceived - start)
+
+
+            System.Threading.Thread.Sleep(1000)
+        Loop
+
+    End Sub
+
+
+
+#End Region
+
+    'load
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '启动位置
+        记住位置ToolStripMenuItem.Checked = My.Settings.savepos
+
+        Me.Left = My.Computer.Screen.Bounds.Width   '1187
+        If 记住位置ToolStripMenuItem.Checked = False Then
+            Me.Top = My.Computer.Screen.Bounds.Height - 140
+        Else
+            Me.Top = My.Settings.pos.Y
+        End If
+
+        NotifyIcon1.Visible = False
+        Me.TransparencyKey = Color.FromArgb(240, 240, 240)
+        Me.Show()
+        '添加菜单
+        For i = 1 To myNA.Length
+            选择网络ToolStripMenuItem.DropDownItems.Add(myNA(i - 1).Name)
+            AddHandler 选择网络ToolStripMenuItem.DropDownItems(i - 1).Click, AddressOf 选择网络事件
+
+        Next
+        选择网络ToolStripMenuItem.DropDownItems(netlist).ForeColor = Color.Red
+
+        Dim data As IPv4InterfaceStatistics = myNA(netlist).GetIPv4Statistics
+        start = data.BytesReceived
+
+        Timer2.Enabled = True
+
+        regedit()
+
+        保存流量数据ToolStripMenuItem.Checked = My.Settings.savedata
+        If My.Settings.savedata = False Then
+            savedata = 0
+        Else
+            savedata = My.Settings.downdata
+        End If
+
+        锁定位置ToolStripMenuItem.Checked = My.Settings.lockpos
+        If 锁定位置ToolStripMenuItem.Checked = False Then
+            归位ToolStripMenuItem.Enabled = True
+        Else
+            归位ToolStripMenuItem.Enabled = False
+        End If
+
+
+
+        Control.CheckForIllegalCrossThreadCalls = False
+        Thread1.IsBackground = True
+        Thread1.Start()
+
+    End Sub
+
+    '关闭保存
+    Private Sub 关闭保存数据(sender As Object, e As EventArgs) Handles Me.FormClosing
+        savedate()
+        Thread1.Abort()
+    End Sub
+
+    Sub savedate()
+        Try
+            My.Settings.lockpos = 锁定位置ToolStripMenuItem.Checked
+            My.Settings.savepos = 记住位置ToolStripMenuItem.Checked
+            If 保存流量数据ToolStripMenuItem.Checked = False Then
+                My.Settings.savedata = False
+                My.Settings.downdata = 0
+            Else
+                Dim data As String = Label4.Text
+                My.Settings.savedata = True
+                My.Settings.downdata = Split(data)(1)
+            End If
+            If 记住位置ToolStripMenuItem.Checked = True Then
+                My.Settings.pos = Me.Location
+                My.Settings.savepos = True
+            Else
+                My.Settings.pos = New Point(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height - 160)
+                My.Settings.savepos = False
+            End If
+        Catch ex As Exception
+        Finally
+            My.Settings.Save()
+        End Try
+    End Sub
+
+    Private Sub 选择网络事件(sender As Object, e As EventArgs)
+        For i = 1 To 选择网络ToolStripMenuItem.DropDownItems.Count
+            选择网络ToolStripMenuItem.DropDownItems(i - 1).ForeColor = Color.Black
+            If CType(sender, ToolStripItem).Text = 选择网络ToolStripMenuItem.DropDownItems(i - 1).Text Then netlist = i - 1
+        Next
+        CType(sender, ToolStripItem).ForeColor = Color.Red
+        My.Settings.netselect = netlist
+        savedate()
+        Application.Restart()
+    End Sub
+
+    Sub regedit()
+        Dim newKey As RegistryKey = My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+
+        If newKey.GetValue("快速启动", "-1") = "-1" Then
+            开机启动ToolStripMenuItem.Checked = False
+        Else
+            开机启动ToolStripMenuItem.Checked = True
+        End If
+    End Sub
+
+#Region "移动窗体"
+    Public X, Y As Integer
+    Public moveflag As Boolean = True
+    Private Sub Form1_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseDown, Label2.MouseDown, Label4.MouseDown
+
+        If moveflag = False Then Exit Sub
+        X = e.X : Y = e.Y
+    End Sub
+    Private Sub Form1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseMove, Label2.MouseMove, Label4.MouseMove
+        If moveflag = False Then Exit Sub
+        If X = e.X And Y = e.Y Then Exit Sub
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+
+            Me.Left = Me.Left + e.X - X
+            Me.Top = Me.Top + e.Y - Y
+
+        End If
+    End Sub
+#End Region
+
+#Region "右击菜单设置"
+    Private Sub 右击弹出菜单(sender As Object, e As MouseEventArgs) Handles Label1.MouseClick, Label2.MouseClick, Label4.MouseClick
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+            ContextMenuStrip1.Show(Label1, -ContextMenuStrip1.Size.Width - 2, 70 - ContextMenuStrip1.Size.Height)
+        End If
+    End Sub
+
+    Private Sub 重启(sender As Object, e As EventArgs) Handles 重启ToolStripMenuItem.Click
+        savedate()
+        Application.Restart()
+    End Sub
+
+    Private Sub 关闭(sender As Object, e As EventArgs) Handles 关闭ToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub 归位(sender As Object, e As EventArgs) Handles 归位ToolStripMenuItem.Click
+        Me.Left = My.Computer.Screen.Bounds.Width - Label1.Width   '1187
+        Me.Top = My.Computer.Screen.Bounds.Height - 140
+    End Sub
+
+    Private Sub 锁定位置button(sender As Object, e As EventArgs) Handles 锁定位置ToolStripMenuItem.Click
+        锁定位置ToolStripMenuItem.Checked = Not 锁定位置ToolStripMenuItem.Checked
+        If 锁定位置ToolStripMenuItem.Checked = True Then
+            moveflag = False
+        Else
+            moveflag = True
+        End If
+    End Sub
+
+    Private Sub 点击穿透ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 点击穿透ToolStripMenuItem.Click
+        点击穿透ToolStripMenuItem.Checked = Not 点击穿透ToolStripMenuItem.Checked
+        If 点击穿透ToolStripMenuItem.Checked = True Then
+            SetWindowLongA(Me.Handle, GWL_EXSTYLE, GetWindowLongA(Me.Handle, GWL_EXSTYLE) Or WS_EX_LAYERED Or WS_EX_TRANSPARENT)
+            NotifyIcon1.Visible = True
+        Else
+            SetWindowLongA(Me.Handle, GWL_EXSTYLE, 0 Or WS_EX_LAYERED)
+            NotifyIcon1.Visible = False
+        End If
+    End Sub
+
+    Private Sub 开机启动(sender As Object, e As EventArgs) Handles 开机启动ToolStripMenuItem.Click
+        开机启动ToolStripMenuItem.Checked = Not 开机启动ToolStripMenuItem.Checked
+        Dim newKey As RegistryKey = My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+        If 开机启动ToolStripMenuItem.Checked = True Then
+            Try
+                newKey.SetValue("快速启动", Application.ExecutablePath, RegistryValueKind.String)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+        Else
+            Try
+                newKey.DeleteValue("快速启动")
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub 自由门ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 自由门ToolStripMenuItem.Click
+        Try
+            Shell("D:\Program File\自由门\goagent\local\goagent.exe", AppWinStyle.Hide)
+
+        Catch ex As Exception
+            MsgBox(ex.Message, 64, "提示信息!")
+        End Try
+    End Sub
+
+    Private Sub P2PToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles P2PToolStripMenuItem.Click
+        Try
+            Shell("D:\备份\我的程序\P2PSearchers6.3.0.exe", AppWinStyle.NormalFocus)
+
+        Catch ex As Exception
+            MsgBox(ex.Message, 64, "提示信息!")
+        End Try
+    End Sub
+
+    Private Sub WIFI共享ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles WIFI共享ToolStripMenuItem.Click
+        Try
+            Process.Start("D:\Program File\PCMaster\magicwifi.exe")
+        Catch ex As Exception
+            MsgBox(ex.Message, 64, "提示信息!")
+        End Try
+
+    End Sub
+
+    Private Sub 保存流量数据(sender As Object, e As EventArgs) Handles 保存流量数据ToolStripMenuItem.Click
+        保存流量数据ToolStripMenuItem.Checked = Not 保存流量数据ToolStripMenuItem.Checked
+    End Sub
+
+    Private Sub 记住位置ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 记住位置ToolStripMenuItem.Click
+        记住位置ToolStripMenuItem.Checked = Not 记住位置ToolStripMenuItem.Checked
+    End Sub
+#End Region
+
+    Private Sub 双击ping(sender As Object, e As EventArgs) Handles Label4.MouseDoubleClick, Ping检测ToolStripMenuItem.Click
+
+        If My.Computer.Network.IsAvailable = True Then
+            Dim 百度 As String
+            百度 = Ping1.Send("www.baidu.com", 1000).RoundtripTime.ToString & " ms"
+
+            Dim 新浪 As String
+            新浪 = Ping1.Send("www.sina.com", 1000).RoundtripTime.ToString & " ms"
+
+            Dim 斗鱼 As String
+            斗鱼 = Ping1.Send("www.douyutv.com", 1000).RoundtripTime.ToString & " ms"
+
+            Dim CSDN As String
+            CSDN = Ping1.Send("www.csdn.net", 1000).RoundtripTime.ToString & " ms"
+
+            Dim acfun As String
+            acfun = Ping1.Send("www.acfun.tv", 1000).RoundtripTime.ToString & " ms"
+
+            Dim sum As String = "百度   Ping:     " + 百度 + vbCrLf + "新浪   Ping:     " + 新浪 + vbCrLf + "斗鱼   Ping:     " + 斗鱼 + vbCrLf + "A站    Ping:     " + acfun
+
+            If NotifyIcon1.Visible = True Then NotifyIcon1.ShowBalloonTip(2000, "Ping检测", sum, ToolTipIcon.Info) : Exit Sub
+            ToolTip1.Show(sum, Label4, 13, -135, 2500)
+        Else
+            If NotifyIcon1.Visible = True Then NotifyIcon1.ShowBalloonTip(2000, "Ping检测", "网络未连接！", ToolTipIcon.Info) : Exit Sub
+
+            ToolTip1.Show("网络未连接！", Label4, 39, -90, 2500)
+        End If
+
+    End Sub
+
+    Private Sub 滑动显示(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If 记住位置ToolStripMenuItem.Checked = True Then
+            If Me.Left <= My.Settings.pos.X Then Timer2.Enabled = False : Me.Left = My.Settings.pos.Y : Exit Sub
+            Me.Left -= 2
+        Else
+            If Me.Left <= My.Computer.Screen.Bounds.Width - Label1.Width Then Timer2.Enabled = False : Me.Left = My.Computer.Screen.Bounds.Width - Label1.Width : Exit Sub
+            Me.Left -= 2
+        End If
+    End Sub
+
+    Private Sub 边缘吸附(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp, Label2.MouseUp
+        If Me.Left + Me.Width > My.Computer.Screen.Bounds.Width - 25 Then
+            Me.Left = My.Computer.Screen.Bounds.Width - Me.Width
+        End If
+    End Sub
+
+
+
+End Class
+
